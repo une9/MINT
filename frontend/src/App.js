7b9886dce19471a074ddbaeb603b512864a25182
eylee1113 <eylee1113@naver.com>
2022-03-23 15:47:34 +0900

    [S06P22A106-28] feat: add carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,24 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./components/navbar/Sidebar";
 import SidebarUSer from "./components/navbar/SidebarUser";
 import SidebarAdmin from "./components/navbar/SidebarAdmin";
+import { useEffect } from "react";
+import { useLocation,useNavigate } from "react-router-dom";
 
 const App= ( ) => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    useEffect(() => {
+    if (pathname==='/') {
+      navigate('/home');
+    }
+  });
     return(
         <div>
             {/* <Sidebar/> */}
             <SidebarUSer/>
             {/* <SidebarAdmin/> */}
             <Outlet />
+
         </div>
     );
 }
