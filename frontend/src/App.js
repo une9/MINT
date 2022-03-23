5adc4706bdcbdca0ad63961b4f2363807295de85
eylee1113 <eylee1113@naver.com>
2022-03-23 18:25:50 +0900

    [S06P22A106-62] wip: login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App= ( ) => {
     }
   });
     return(
-        <div>
+        <div style={{height:"100%"}}>
             <Sidebar/>
             {/* <SidebarUSer/> */}
             {/* <SidebarAdmin/> */}
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,41 @@
-const Login= ( ) => {
-    return(
-       <div>Login</div>
-    );
-}
+import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+import Mint_Lodo from '../lottie/Mint_Logo_Long_Font.json';
+import styles from '../styles/Login.scss';
+const lottieOptions = {
+  animationData: Mint_Lodo,
+  loop: true,
+  autoplay: true,
+  rendererSettings: {
+    className: 'add-class', // svg에 적용
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+const Login = () => {
+  const [isStopped, SetIsStopped] = useState(false);
+  const [isPaused, SetIsPaused] = useState(true);
+  return (
+    <div className="login" style={styles}>
+      <div className="left_area">
+        <div>
+          <Lottie
+            className="logo_lottie"
+            options={lottieOptions}
+            isStopped={isStopped}
+            isPaused={isPaused}
+            isClickToPauseDisabled={false}
+            style={{ width: '550px', height: '550px' }} // svg의 부모 div에 적용
+            eventListeners={[
+              {
+                eventName: 'complete',
+                callback: () => console.log('the animation completed'),
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="right_login">흠?</div>
+    </div>
+  );
+};
 export default Login;
