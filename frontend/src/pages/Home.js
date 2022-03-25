7fbd43415565509edae4768a836dfccb4fa60779
eylee1113 <eylee1113@naver.com>
2022-03-25 13:35:31 +0900

    [S06P22A106-62] wip: connect detail page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,32 +1,53 @@
 import styles from '../styles/Home.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Lottie from 'react-lottie';
 import { Carousel } from 'react-bootstrap';
 import Kepler_1649C from '../lottie/Planet_Kepler_1649C.json';
 import Kepler_22B from '../lottie/Planet_Kepler_22B.json';
+import axios from 'axios';
 
-const lottieOptions1 = {
-  animationData: Kepler_1649C,
-  loop: true,
-  autoplay: true,
-  rendererSettings: {
-    className: 'add-class', // svg에 적용
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-const lottieOptions2 = {
-  animationData: Kepler_22B,
-  loop: true,
-  autoplay: true,
-  rendererSettings: {
-    className: 'add-class', // svg에 적용
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
+
 const Home = () => {
+  const [planetList, setPlanetList] = useState();
   const [isStopped, SetIsStopped] = useState(false);
   const [isPaused, SetIsPaused] = useState(true);
 
+  useEffect(()=>{
+    axios
+    .get(process.env.REACT_APP_SERVER_URL +"/api/planets",{})
+    .then((res)=>{
+
+        const planetData = res.data.planet;
+        setPlanetList((prevState) => ({
+            ...prevState,
+            planetData,
+        }));
+        console.log(planetData);
+      
+    })
+  },[]);
+  const lottieOptions1 = {
+    animationData: Kepler_1649C,
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+      className: 'add-class', // svg에 적용
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+  const lottieOptions2 = {
+    animationData: Kepler_22B,
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+      className: 'add-class', // svg에 적용
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+  
+  const movepage = ()=>{
+    
+  }
   return (
     <div className="main" style={styles}>
       {/* <div> */}
@@ -38,7 +59,7 @@ const Home = () => {
               options={lottieOptions1}
               isStopped={isStopped}
               isPaused={isPaused}
-              isClickToPauseDisabled={false}
+              onClick={movepage}
               style={{ width: '500px', height: '500px' }} // svg의 부모 div에 적용
               eventListeners={[
                 {
@@ -54,7 +75,7 @@ const Home = () => {
               options={lottieOptions2}
               isStopped={isStopped}
               isPaused={isPaused}
-              isClickToPauseDisabled={false}
+              onClick={movepage}
               style={{ width: '500px', height: '500px' }} // svg의 부모 div에 적용
               eventListeners={[
                 {
