00021840f552e738169b1fbef2b69f723f220c76
eylee1113 <eylee1113@naver.com>
2022-03-31 10:26:25 +0900

    [S06P22A106-62] feat: scss fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,38 +7,14 @@ import { useEffect, useState } from "react";
 import { useLocation,useNavigate } from "react-router-dom";
 
 
-let currentAccount;
-
-const checkWalletIsConnected = async () => {
-  const { ethereum } = window;
-
-  if (!ethereum) {
-      console.log("Wallet is not exist");
-      return;
-  } else {
-      console.log("Wallet exist!");
-  }
-
-  const accounts = await ethereum.request({ method: 'eth_accounts'});
-
-  if (accounts.length !== 0){
-      const account = accounts[0];
-      currentAccount = account;
-      console.log("현재 연결된 지갑 주소 : ", account);
-  } else {
-      console.log("현재 연결된 지갑 X");
-  }
-}
-
 const App= ( ) => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     useEffect(() => {
-    checkWalletIsConnected();
-    if (pathname==='/') {
-      navigate('/home');
-    }
-  });
+      if (pathname==='/') {
+        navigate('/home');
+      }
+    });
     return(
         <div style={{height:"100%"}}>
           {isLogin()?(
--- frontend/src/utils/isLogin.js
+++ frontend/src/utils/isLogin.js
@@ -1,11 +1,39 @@
-const isLogin = () => {
-    const account = localStorage.getItem("account");
+
+import { useEffect, useState } from "react";
+const IsLogin = () => {
+
+    // const account = localStorage.getItem("account");
     // console.log(token);
-    if (account === null) {
-      // console.log(token);
-      return false;
-    } else {
-      return true;
+    // if (account === null) {
+    //   // console.log(token);
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+    useEffect(() => {
+      checkWalletIsConnected();
+    });
+    const [currentAccount, setCurrentAccount] = useState();;
+
+    const checkWalletIsConnected = async () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+          console.log("Wallet is not exist");
+          return;
+      } else {
+          console.log("Wallet exist!");
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+      if (accounts.length !== 0){
+          const account = accounts[0];
+          setCurrentAccount(account);
+          console.log("현재 연결된 지갑 주소 : ", account);
+      } else {
+          console.log("현재 연결된 지갑 X");
+      }
     }
   };
-  export default isLogin;
+  export default IsLogin;
