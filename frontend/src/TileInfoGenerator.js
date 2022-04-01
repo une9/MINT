9e624a3918e98aa0ea7b487bbbb86d7011c3066f
une9 <59572449+une9@users.noreply.github.com>
2022-04-01 11:43:53 +0900

    wip

--- frontend/src/TileInfoGenerator.js
+++ frontend/src/TileInfoGenerator.js
@@ -1,9 +1,18 @@
+const planetID = {
+    "Teegarden_b": 9,
+    "Kepler_1649c": 10,
+     "Ross_128b": 11,
+     "Kepler_22b": 12,
+     "Proxima_B": 13
+}
+
 const TileInfoGenerator = (planetCode, blocks, largeTiles) => {
     let res = "";
     for (let i=0; i < blocks.length; i++) {
         const blockName = String.fromCharCode(65 + i);
         const tileIds = new Array(blocks[i]).fill("").map((tile, idx) => `${planetCode}-${blockName}-${idx+1}`);
-        // res += tileIds.map((id, idx) =.)
+        // const insertStr = tileIds.map((id, idx) => `INSERT INTO Tile(tile_id, area, price, planet_id) VALUES(${}, ${}, ${}, ${})`);
+        // res += insertStr
     }
 }
 
